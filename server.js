ac1abd49e69574879220e70fc230f5ab2066abdf
Louis Alridge <loualrid@gmail.com>
2017-09-26 16:33:40 -0700

    Resetting state of master

--- server.js
+++
@@ -1,9 +0,0 @@
-var webpack = require('webpack')
-var WebpackDevServer = require('webpack-dev-server')
-var config = require('./webpack.config')
-
-new WebpackDevServer(webpack(config), {
-  historyApiFallback: true
-}).listen(3000, '0.0.0.0', function (err, res) {
-  err ? console.log(err) : console.log("Listening at 0.0.0.0:3000")
-})
