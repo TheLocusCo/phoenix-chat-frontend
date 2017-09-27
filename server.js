4e580b37471c338453860f03b9eb8abf54af9c0d
Louis Alridge <loualrid@gmail.com>
2017-09-26 17:28:34 -0700

    Hello world and config

---
+++ server.js
@@ -0,0 +1,11 @@
+var webpack = require('webpack')
+var WebpackDevServer = require('webpack-dev-server')
+var config = require('./webpack.config')
+
+new WebpackDevServer(webpack(config), {
+  historyApiFallback: true,
+  public: '192.168.195.137',
+  disableHostCheck: true
+}).listen(3000, '0.0.0.0', function (err, res) {
+  err ? console.log(err) : console.log("Listening at 0.0.0.0:3000")
+})
